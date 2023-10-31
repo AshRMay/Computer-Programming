10046117178d5e2da84c3e85cb0cbf77002d8bca
Scholar <scholar@paradigm.local>
2023-10-31 14:55:33 -0600

    IntroToJavaScript

---
+++ IntroToJavaScript.js
@@ -0,0 +1,24 @@
+const myName = "Ash"
+
+let favColor = "Red"
+
+favColor = "Blue"
+
+let age = 30
+
+mainPerson = {
+    name: "Bob",
+    age: 30,
+    favColor: "Blue"
+}
+
+favFoood = ["Steak", "Ice Cream", "Chicken Nuggets"]
+
+const isAlive = true
+
+function describePerson(person) {
+    alert("MISSSING PERSON ALERT:" + person.name);
+    alert(`MISSING PERSON ALERT: ${person.name} missing since 1988, wearing a sombrero.`)
+}
+
+describePerson(mainPerson)

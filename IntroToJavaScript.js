84f4364a811baaa5fd6da7b313769cf6f9517d91
Scholar <scholar@paradigm.local>
2023-11-07 14:58:56 -0700

    Variables&DataTypes

--- IntroToJavaScript.js
+++ IntroToJavaScript.js
@@ -1,24 +1,37 @@
+document.getElementById("title").innerHTML = `Hello ${myName}`;
+
 const myName = "Ash"
 
-let favColor = "Red"
+ let favColor = "Red"
 
-favColor = "Blue"
+ favColor = "Blue"
 
-let age = 30
+ let age = 30
 
 mainPerson = {
-    name: "Bob",
-    age: 30,
-    favColor: "Blue"
+     name: "Ash",
+     age: 30,
+     favColor: "Blue"
+ }
+
+ favFoood = ["Steak", "Ice Cream", "Chicken Nuggets"]
+
+ const isAlive = true
+
+ function describePerson(person) {
+     alert("MISSSING PERSON ALERT:" + person.name);
+     alert(`MISSING PERSON ALERT: ${person.name} missing since 1988, wearing a sombrero.`)
+ }
+
+ describePerson(mainPerson)
+
+ if (age > 30 && age < 50){/     alert("You're on your way to middle age.")
+ } else if ( age < 30) {
+     alert("You're a baby.")
+ } else {
+     alert("How are you alive...?")
 }
 
-favFoood = ["Steak", "Ice Cream", "Chicken Nuggets"]
-
-const isAlive = true
-
-function describePerson(person) {
-    alert("MISSSING PERSON ALERT:" + person.name);
-    alert(`MISSING PERSON ALERT: ${person.name} missing since 1988, wearing a sombrero.`)
-}
-
-describePerson(mainPerson)
+const para = document.createElement("p");
+para.innnerHTML = "I'm the paragrah you just created.";
+document.getElementsByTagName("div1").appendChild(para);
---
+++ Variables&DataTypes/Variables&DataTypes.js
@@ -0,0 +1,40 @@
+document.getElementById("title").innerHTML = `Name? ${theName}`;
+
+const theName = "Bob"
+const speciesWhat = "Human"
+let theRole = "Crewmate"
+theRole = "Imposter"
+let age = 25
+let kills = 3
+const birthYear = 2020
+const isAlive = true
+let beenFound = false
+const hasAllies = true
+victimsNames = ["Pablo", "Fred", "Lester"]
+alliesNames = ["Sophie", "Steve", "Rick"]
+weaponArsenal = ["M1911", "Ballistic Knife", "C4"]
+
+personOne = {
+    name: "Bob",
+    species: "Human",
+    role: "Imposter",
+    age: 25,
+    kills: 3,
+    birthday: 2020
+}
+personTwo = {
+    name: "Sophie",
+    species: "Human",
+    role: "Imposter",
+    age: 21,
+    kills: 2,
+    birthday: 2024
+}
+personThree = {
+    name: "Rick",
+    species: "Human",
+    role: "Imposter",
+    age: 17,
+    kills: 4,
+    birthday: 2028
+}

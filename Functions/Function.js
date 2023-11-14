bb6b2e4a97df6f9ab7e2f3e1a814741e1246f6cc
Scholar <scholar@paradigm.local>
2023-11-14 14:54:48 -0700

    Functions

--- Functions/Function.js
+++ Functions/Function.js
@@ -3,34 +3,39 @@ function tellFortune() {
 
     numOfChildren = document.getElementById('children').value;
     partnerName = document.getElementById('partner').value;
-    location = document.getElementById('location').value;
     jobTitle = document.getElementById('job').value;
 
     document.getElementById('You will be an ${jobTitle} in  ${location}, and married to ${partnername} with ${numOfChildren} kids.');
 }
 function calculateDogAge(puppyAge) {
+    puppyAge = document.getElementById("dog-age").value;
     dogAge = puppyAge * 7;
-    console.log('Your doggie is ${dogAge} years old in dog years!')
+    document.getElementById(
+    "age"
+    ).innerHTML = ('Your doggie is ${dogAge} years old in dog years!')
 }
 function calculateSupply(age, amountPerDay) {
+    age = document.getElementById("age").value
+    amountPerDay = document.getElementById("amount-per-day").value
     maxAge = 80;
     amountPetYear = amountPerDay * 365.25;
     totalAmount = (maxAge - age) * amountPerYear;
     console.log('You will need ${totalAmount} to last you until the ripe old age of ${maxAge}')
 }
-function calculateCircumfrence(radius) {
+function calculateCircumference(radius) {
+    radius = document.getElementById("raidus").value;
     circumference = 2 * Math.PI * radius;
-    console.log('The circumfrence is ${circumfrence}');
+    document.getElementById(
+        "circumference"
+    ).innerHTML = 'The circumference is ${circumference}';
 }
 const calcArea = (radius) => console.log('The area is ${Math.PI * radius * radius}')
 function celsiusToFahrenheit(celsius){
     console.log('${celsius}*C is ${fahrenheit}*F');
 }
 
-const celsiusToFahrenheit = (clesius) => console.log('${celsius}*C is ${ceelsius * 9 / 5 + 32}*F');
-
-function fahrenheitToCelsius(fahrenheit) {
-    clesius = (fahrenheit - 32) * 5 / 9;
-    console.log('$fahrenheit}*F is ${celsius}*C');
+function clesiusToFahrenheit() {
+    celsius = document.getElementById("celsius").value;
+    fahrenheit = (celsius * 9) / 5 + 32;
+    document.getElementById("fahrenheit").innerHTML = '${celsius}C is ${fahrenheit}F';
 }
-const fahrenheitToCelsius = (fahrenheit) => console.log('${fahrenheit}*F')
